656b4b54e33f1092150010e9f6cfc24b684eb628
Prashant_Rao <prashantraogunjal1562@gmail.com>
2022-09-29 18:56:08 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/common/header/Header"
+import "./App.css"
+import Homepages from "./components/home/Homepages"
+import Footer from "./components/common/footer/Footer"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Culture from "./components/culture/Culture"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepages} />
+        
+          <Route exact path='/culture' component={Culture} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB paddingTB'>
+          <div className='logo'>
+            <img src='../images/Hindustan_Times.png' alt='' />
+          </div>
+          <div className='ad'>
+            <img src='../images/.png' alt='' />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head;
